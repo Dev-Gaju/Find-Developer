77148e5bf9caf81e5d379c57048e165d82844441
Dev-Gaju <gajuahmd@gmail.com>
2022-06-14 16:52:30 +0600

    check commit

--- StudyTest/urls.py
+++ StudyTest/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
-
 urlpatterns = [
+    path('abc/', include('projects.urls')),
     path('admin/', admin.site.urls),
-    path(' ', include('projects.urls'))
+
 
 ]
