9f95487c7efed2d2e3e6c7419c0b19adff8c074a
Dev-Gaju <gajuahmd@gmail.com>
2022-07-09 12:16:55 +0600

    check update

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project1'),
+    path('create-project', views.creteProject, name= 'create-project'),
+    path('update-project/<str:pk>/', views.UpdateProject, name= 'update-project'),
 
 ]
