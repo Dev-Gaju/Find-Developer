730bc63c72ca8f895d62d7830c9ee8cb91a18005
Dev-Gaju <gajuahmd@gmail.com>
2022-08-21 22:12:42 +0600

    authenication update

--- Django/StudyTest/users/views.py
+++ Django/StudyTest/users/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from .models import Profiles
 
 
-# Create your views here.
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('Username does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)  # this create a session for the user in database
+            return redirect('profiles')
+        else:
+            print("User or password is incorrect ")
+
+    return render(request, 'users/login_register.html')
+
 
 def profiles(request):
     profiles = Profiles.objects.all()
